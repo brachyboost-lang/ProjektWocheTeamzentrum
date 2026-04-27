3c76d423f800e75a939bb55e2d95b18d45ccc215
Alexander Lang <Alexander.Lang@it-lang.de>
2026-04-27 10:40:09 +0200

    EventVM started

---
+++ ProjektWocheTeamzentrum/ViewModels/EventVM.cs
@@ -0,0 +1,47 @@
+﻿using ProjektWocheTeamzentrum.Models.Events;
+using ProjektWocheTeamzentrum.Models.Users;
+using ProjektWocheTeamzentrum.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+
+namespace ProjektWocheTeamzentrum.ViewModels
+{
+    public class EventVM
+    {
+        public ObservableCollection<Event> Events { get; set; } = new ObservableCollection<Event>();
+        public List<User> RegisteredParticipants { get; set; } = new List<User>();
+        public int EventId { get; set; }
+        public DateTime StartingTime { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int DurationInMinutes { get; set; }
+        public string MeetingLocation { get; set; } = string.Empty;
+        public int RequiredClearanceLevel { get; set; } = 0;
+        public int IdHandler { get; set; } = 0;
+
+        public async Task InitalizeEvents()
+        {
+            try
+            {
+                List<Event> events = new List<Event>();
+                events = await EventUtil.GetAllEventsAsync();
+                foreach (Event e in events)
+                {
+                    Events.Add(e);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public EventVM()
+        {
+            _ = InitalizeEvents();
+        }
+    }
+}
